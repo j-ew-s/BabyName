664b8914dc85fb808ef4be3f5c6c0fb32af38521
j-ew-s <gabriel.scavassa@outlook.com>
2015-10-06 17:01:16 -0300

    Creating routes: About and Home

--- app/js/module.js
+++ app/js/module.js
@@ -1,6 +1,34 @@
-var babyApp = angular.module('babyApp',[]);
+var babyApp = angular.module('babyApp',['ngRoute']);
+
+babyApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'views/home.html',
+                controller  : 'mainCtrl'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'views/about.html',
+                controller  : 'aboutCtrl'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'app/views/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
 
 babyApp.controller('mainCtrl', function($scope){
 	
 	$scope.name = "Vote for baby's name today!!"
+});
+
+babyApp.controller('aboutCtrl', function($scope){
+	
+	$scope.name = "Web page for my baby's name vote counting!"
 });
