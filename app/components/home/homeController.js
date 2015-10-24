9f6c0359f2acf941bd65c1a4ef57663cd2c40d8f
j-ew-s <gabriel.scavassa@outlook.com>
2015-10-24 10:29:36 -0200

    trying Charts- adjustments

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -14,66 +14,73 @@
 	
 		//data to chart
 		homeFactory.getChartData().then(function(promise) {
-			console.log('promise',promise.data.data)
-   			 $scope.chart = promise.data.data;
-   			 var chartValues;
-   			 for(var i = 0; i > promise.data.data.length; i++ )
-   			 {
-   			 	chartValues += "['" + promise.data.data[i].Name + "' , " + promise.data.data[i].Vote + "]";
-   			 }
+			console.log('promise',promise.data.data);
+			$scope.chart = promise.data.data;
+			var chartValues = [];
+			var baby = "";
+			for(var i = 0; i < promise.data.data.length; i++ )
+			{
+			 	
+			 	baby = "['" + promise.data.data[i].Name + "' , '" + promise.data.data[i].Vote + "']";
+			 	console.log('entrou', baby);
+			 	chartValues.push(baby);
+			 	console.log('!!chartValues',chartValues);
+			 	baby = "";
+			}
 
    			 console.log("values chart,",	chartValues);
    			 $('#container').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'World\'s largest cities per 2014'
-        },
-        subtitle: {
-            text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
-        },
-        xAxis: {
-            type: 'category',
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Population (millions)'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
-        },
-        series: [{
-            name: 'Population',
-            data: [
-               chartValues
-            ],
-            dataLabels: {
-                enabled: true,
-                rotation: -90,
-                color: '#FFFFFF',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 10, // 10 pixels down from the top
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        }]
-    });
+		        chart: {
+		            type: 'column'
+		        },
+		        title: {
+		            text: 'World\'s largest cities per 2014'
+		        },
+		        subtitle: {
+		            text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+		        },
+		        xAxis: {
+		            type: 'category',
+		            labels: {
+		                rotation: -45,
+		                style: {
+		                    fontSize: '13px',
+		                    fontFamily: 'Verdana, sans-serif'
+		                }
+		            }
+		        },
+		        yAxis: {
+		            min: 0,
+		            title: {
+		                text: 'Population (millions)'
+		            }
+		        },
+		        legend: {
+		            enabled: false
+		        },
+		        tooltip: {
+		            pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
+		        },
+		        series: [{
+		            name: 'Population',
+		            data: [{
+		               chartValues
+		            }
+		            ],
+		            dataLabels: {
+		                enabled: true,
+		                rotation: -90,
+		                color: '#FFFFFF',
+		                align: 'right',
+		                format: '{point.y:.1f}', // one decimal
+		                y: 10, // 10 pixels down from the top
+		                style: {
+		                    fontSize: '13px',
+		                    fontFamily: 'Verdana, sans-serif'
+		                }
+		            }
+		        }]
+		    });
   		});
 
 	}]);
